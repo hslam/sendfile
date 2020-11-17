874ba37621051fc11828d5d7081450dfa0936069
hslam <791874158@qq.com>
2020-11-18 00:26:49 +0800

    add windows

---
+++ sendfile_windows.go
@@ -0,0 +1,11 @@
+// Copyright (c) 2020 Meng Huang (mhboy@outlook.com)
+// This package is licensed under a MIT license that can be found in the LICENSE file.
+
+// +build windows
+
+package sendfile
+
+// SendFile wraps the sendfile system call.
+func SendFile(conn net.Conn, src int, pos, remain int64) (written int64, err error) {
+	return sendFile(conn, src, pos, remain)
+}
